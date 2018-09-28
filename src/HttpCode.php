687a97ac0039ea7fc00107c870589b3d7c11808a
Programster <sdpagent@gmail.com>
2018-09-28 15:52:59 +0100

    filling in docblock for consts

--- src/HttpCode.php
+++ src/HttpCode.php
@@ -4,15 +4,19 @@ namespace iRAP\Http;
 
 class HttpCode
 {
-    /*
-     * Response for good requests
+    /**
+     * Response for OK.
      */
     const OK = 200;
     
-    
+    /**
+     * Response for if the user made an invalid request.
+     */
     const BAD_REQUEST = 400;
     const REDIRECT_PERMANENT = 301;
     const REDIRECT_TERMPORARY = 302;
+    
+    
     const FORBIDDEN = 403;
     const NOT_FOUND = 404;
     const SERVER_ERROR = 500;

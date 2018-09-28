b3ab1da88914dedef269252978f0c1c9f8facb2f
Stuart <stuart.page@irap.org>
2018-09-28 14:47:25 +0000

    Update HttpCode.php

--- src/HttpCode.php
+++ src/HttpCode.php
@@ -4,7 +4,12 @@ namespace iRAP\Http;
 
 class HttpCode
 {
+    /*
+     * Response for good requests
+     */
     const OK = 200;
+    
+    
     const BAD_REQUEST = 400;
     const REDIRECT_PERMANENT = 301;
     const REDIRECT_TERMPORARY = 302;

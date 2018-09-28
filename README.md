57916a2ec7affcf8dd1987e4cd7bf0d112c1bb7c
Programster <sdpagent@gmail.com>
2018-09-28 15:25:10 +0100

    Filling in composer.json file for the package and actually filling in the code

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 Awesome HTTP Codes Package
 ==========================
 
-balahh
+To use this package of defines you need to manually include the
+
+## Usage
+
+```
+return iRAP/Http/HttpCode::OK;
+```
+
+Alternatively:
+```
+use iRAP/Http/HttpCode;
+
+return HttpCode::OK;
+```
---
+++ src/HttpCode.php
@@ -0,0 +1,18 @@
+
+
+namespace iRAP\Http;
+
+class HttpCode
+{
+    const OK = 200;
+    const BAD_REQUEST = 400;
+    const REDIRECT_PERMANENT = 301;
+    const REDIRECT_TERMPORARY = 302;
+    const FORBIDDEN = 403;
+    const NOT_FOUND = 404;
+    const SERVER_ERROR = 500;
+}
+
+
+
+
